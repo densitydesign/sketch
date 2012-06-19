4659f09a342f512ae580cc95a318aadab39b3412
Mauro Bianchi <bianchimro@gmail.com>
2012-06-19 02:09:33 +0200

    moved mongo wrapper to separated file and renamed class from MongoHandler to MongoWrapper

--- sketchproject/sketch/views.py
+++ sketchproject/sketch/views.py
@@ -12,91 +12,7 @@ import json
 import bson
 import bson.json_util
 
-class MongoHandler(object):
-    """
-    This class interacts with Mongo Db instance 
-    """
-    
-    available_commands = ['find_one', 'find', 'count']
-    
-    def __init__(self, server=None, port=None):
-        self.server = server or settings.MONGO_SERVER_HOSTNAME
-        self.port = int(port or settings.MONGO_SERVER_PORT)
-        self.connection = None
-        
-    def connect(self):
-    
-        self.connection = pymongo.Connection(self.server, self.port)
-        
-        
-    def getDb(self, db_name):
-    
-        db = getattr(self.connection, db_name)
-        return db
-        
-        
-    def getCollection(self, db_name, collection_name):
-    
-        db = self.getDb(db_name)
-        collection = getattr(db, collection_name)
-        return collection
-        
-        
-    def _insert(self, db_name, collection_name, document):
-    
-        collection = self.getCollection(db_name, collection_name)
-        return collection.insert(document)    
-    
-    
-    def insert(self, db_name, collection_name, request):
-    
-        document = request.GET.get('document') or request.POST.get('document')
-        document = self.parseJsonDict(document)       
-        return self._insert(db_name, collection_name, document)
-    
-    
-    def find_one(self, db_name, collection_name, request): 
-    
-        queryDict = request.GET.get('query') or request.POST.get('query')
-        queryDict = self.parseJsonDict(queryDict)
-        collection = self.getCollection(db_name, collection_name)
-        return collection.find_one(queryDict)
-        
-        
-    def find(self, db_name, collection_name, request): 
-        
-        queryDict = request.GET.get('query') or request.POST.get('query')
-        queryDict = self.parseJsonDict(queryDict)
-
-        collection = self.getCollection(db_name, collection_name)
-        cursor = collection.find(queryDict)
-
-        out = []
-        for r in cursor:
-            out.append(r)
-        return out
-        
-        
-    def count(self, db_name, collection_name, request): 
-        
-        queryDict = request.GET.get('query') or request.POST.get('query')
-        queryDict = self.parseJsonQueryDict(queryDict)
-        
-        collection = self.getCollection(db_name, collection_name)
-        
-        if queryDict:
-            return collection.find(queryDict).count()
-
-        return collection.count()
-        
-        
-    def parseJsonDict(self, jsonString):
-        #TODO: handle a list of dicts        
-        try:
-            obj = json.loads(jsonString)
-            return dict(obj)
-        except:
-            return {}
+from mongowrapper import MongoWrapper
 
 
 
@@ -122,19 +38,19 @@ def api_call(request, collection, command, database=None):
 
     database = database or settings.MONGO_SERVER_DEFAULT_DB
     
-    handler = MongoHandler()
+    mongo = MongoWrapper()
     
-    commandMethod = getattr(handler, command, None)
-    if not commandMethod or command not in handler.available_commands:
+    commandMethod = getattr(mongo, command, None)
+    if not commandMethod or command not in mongo.available_commands:
         raise Exception("Command %s not supported" % command)
 
-    handler.connect()
+    mongo.connect()
     
     results = commandMethod(database, collection, request)
     if results:
         out['results'] = results
     
-    handler.connection.close()
+    mongo.connection.close()
     return HttpResponse(json.dumps(out, default=bson.json_util.default))
     
 
@@ -156,8 +72,8 @@ def mapper_call(request, collection, database=None):
     out = createBaseResponseObject()
     
     database = database or settings.MONGO_SERVER_DEFAULT_DB
-    handler = MongoHandler()
-    handler.connect()
+    mongo = MongoWrapper()
+    mongo.connect()
     
     #mapping should come from url, in form of id
     mapping = { '__key__' : 'id', 
@@ -173,9 +89,9 @@ def mapper_call(request, collection, database=None):
         data = json.loads(request.raw_post_data)
         for d in data:
             newRecord = mappingManager.mapRecord(d, mapping)
-            handler._insert(database, collection, newRecord)
+            mongo._insert(database, collection, newRecord)
             out['results'].append(newRecord)
     
-    
+    mongo.connection.close()
     return HttpResponse(json.dumps(out, default=bson.json_util.default))
     
