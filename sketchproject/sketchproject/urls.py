d6075ecd4e8ff64780356e71418209eaf51f92ec
Mauro Bianchi <bianchimro@gmail.com>
2012-06-28 11:10:12 +0200

    added static file serving while in debug

--- sketchproject/sketchproject/urls.py
+++ sketchproject/sketchproject/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.conf import settings
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -18,3 +19,14 @@ urlpatterns = patterns('',
     url(r'^sketch/', include('sketch.urls')),
     
 )
+
+
+if settings.DEBUG:
+    urlpatterns = patterns('',
+    url(r'^media/(?P<path>.*)$', 'django.views.static.serve',
+        {'document_root': settings.MEDIA_ROOT, 'show_indexes': True}),
+    url(r'', include('django.contrib.staticfiles.urls')),
+) + urlpatterns
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+urlpatterns += staticfiles_urlpatterns()
