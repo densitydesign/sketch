53ad79d81e418e383ec305e586248b223280c82b
Mauro Bianchi <bianchimro@gmail.com>
2012-06-28 11:11:37 +0200

    adding first view to sketch_ui

--- sketchproject/sketch_ui/views.py
+++ sketchproject/sketch_ui/views.py
@@ -1,1 +1,10 @@
 # Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404, render_to_response
+from django.template import RequestContext, loader
+
+def index(request):
+    c = RequestContext(request)
+    return render_to_response("sketch_ui/index.html", {}, context_instance = c)
+    
+    
