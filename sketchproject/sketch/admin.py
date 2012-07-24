3144b4b56b45cd2b066738f0dbf9deb8fdd28caa
Mauro Bianchi <bianchimro@gmail.com>
2012-07-24 18:24:50 +0200

    registered Sketch mapper for admin

--- sketchproject/sketch/admin.py
+++ sketchproject/sketch/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from models import SketchCollection, SketchCollectionPermission
+from models import SketchCollection, SketchCollectionPermission, SketchMapper
 
 admin.site.register(SketchCollection)
 admin.site.register(SketchCollectionPermission)
+admin.site.register(SketchMapper)
