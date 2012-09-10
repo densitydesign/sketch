841418e111c06d072b7107936c2cba61e31880bc
Mauro Bianchi <bianchimro@gmail.com>
2012-09-10 14:38:55 +0200

    added missing sketchmappers file

---
+++ sketchproject/sketch/sketchmappers.py
@@ -0,0 +1,18 @@
+class MapperCollection(object):
+
+    def __init__(self):
+        self.mappers = dict()
+    
+
+
+mappersCollection = MapperCollection()
+
+
+class CodedMapper(object):
+    mapper = dict()
+
+
+##classes here
+
+class TestMapper(CodedMapper):
+    mapper = { }
