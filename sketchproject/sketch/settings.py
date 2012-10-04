6718e01594c827fdf8955f9c2e11cdff2fe725ab
Mauro Bianchi <bianchimro@gmail.com>
2012-10-04 15:20:44 +0200

    Comments in recordparser.py and moved ALLOWED_PARSERS to settings

--- sketchproject/sketch/settings.py
+++ sketchproject/sketch/settings.py
@@ -8,3 +8,12 @@ MONGO_SERVER_DEFAULT_DB = getattr(settings, 'MONGO_SERVER_DEFAULT_DB', 'sketchdb
 
 DEFAULT_QUERY_LIMIT = 100
 MAX_ERROR_RECORDS = getattr(settings, 'MAX_ERROR_RECORDS', 10000)
+
+
+_ALLOWED_PARSERS = {
+    #'csv' : 'CSVRecordParser', 
+    'jsonlist' : 'JSONListParser',
+    'jsontext' : 'JSONTextFileParser'
+}
+
+ALLOWED_PARSERS = getattr(settings, "ALLOWED_PARSERS", _ALLOWED_PARSERS)
